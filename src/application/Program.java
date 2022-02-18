9468762f3454e3877e7a017d2b08e8604145311d
William Marques <junior.stuart34@gmail.com>
2022-02-18 13:47:15 -0400

    created and tested Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d = new Department(1, "Games");
-		System.out.println(d);
+		Seller s = new Seller(210, "Test Test", "test@gmail.com", new Date(), 3500.0, d);
+		
+		System.out.println(s);
 		
 	}
 
